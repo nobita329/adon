4a552b7c27f3d8070cc2372cb07e8fd9f7380104
𝙏𝙝𝙚 𝘾𝙤𝙙𝙞𝙣𝙜 𝙝𝙪𝙗 <nn8109239@gmail.com>
2026-03-18 23:55:54 +0530

    Update email.sh

--- email.sh
+++ email.sh
@@ -1,11 +1,19 @@
-cd /var/www/pterodactyl
 git clone https://github.com/nobita329/adon.git
 cd adon
 cd Email
+sudo mv /root/adon/Email/1.zip /var/www/pterodactyl/
+sudo mv /root/adon/Email/app.php /var/www/pterodactyl/config/
+sudo mv /root/adon/Email/admin.blade.php /var/www/pterodactyl/resources/views/layouts/
+sudo mv /root/adon/Email/AccountCreated.php /var/www/pterodactyl/app/Notifications/
+sudo mv /root/adon/Email/SendPasswordReset.php /var/www/pterodactyl/app/Notifications/
+sudo mv /root/adon/Email/AddedToServer.php /var/www/pterodactyl/app/Notifications/
+sudo mv /root/adon/Email/RemovedFromServer.php /var/www/pterodactyl/app/Notifications/
+sudo mv /root/adon/Email/ServerInstalled.php /var/www/pterodactyl/app/Notifications/
+sudo mv /root/adon/Email/MailTested.php /var/www/pterodactyl/app/Notifications/
 
 
-
-
+cd /var/www/pterodactyl
+unzip 1.zip
 COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
 php artisan migrate --seed --force
 php artisan view:clear
